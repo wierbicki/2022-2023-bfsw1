5b6a4042eba2214ac63404ba35895aa0d0c4bf03
Daniel Wierbicki <daniel@wierbicki.org>
2023-02-13 09:45:09 +0100

    Ordner mit heutigen Übungen erstellt.

---
+++ 2023-02-13-formular/formular.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $vorname = $_GET["eingabe"];
+    echo "Hallo $vorname";
+    ?>
+</body>
+
+</html>
